67e63bddb6743519e11f4ebe23138b8b73b420f3
Artem P <aopoltorzhicky@gmail.com>
2019-04-03 20:45:13 +0300

    Private API: all methods

--- examples/rest/main.go
+++ examples/rest/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	api := rest.New("", "")
-	data, err := api.GetSpread("ADAETH", 0)
+	api := rest.New("pZai5d5lPFm4uE+raQlSHCRRqNE4MoPU/k1C8nu0/0bZZ9+iItHsiWoc", "iNYlYHYf5j4tE8EvSV+HN6VsJyX3dhkRlw/6+NLLHzDZ+9aA8j/o3ze9gpXDXyEZVnz88vtwsqF8fPZWv82egw==")
+	data, err := api.GetTradeVolume(true, "XXBTZUSD", "XETHZEUR")
 	if err != nil {
 		log.Fatalln(err)
 	}
--- rest/const.go
+++ rest/const.go
@@ -24,10 +24,84 @@ const (
 const (
 	TradeBuy  = "b"
 	TradeSell = "s"
+	Buy       = "buy"
+	Sell      = "sell"
 )
 
 // Order types
 const (
 	TradeLimit  = "l"
 	TradeMarket = "m"
+	Limit       = "limit"
+	Market      = "market"
+)
+
+// Assets
+const (
+	ADA  = "ADA"
+	BCH  = "BCH"
+	BSV  = "BSV"
+	DASH = "DASH"
+	EOS  = "EOS"
+	GNO  = "GNO"
+	KFEE = "KFEE"
+	QTUM = "QTUM"
+	USDT = "USDT"
+	XDAO = "XDAO"
+	XETC = "XETC"
+	XETH = "XETH"
+	XICN = "XICN"
+	XLTC = "XLTC"
+	XMLN = "XMLN"
+	XNMC = "XNMC"
+	XREP = "XREP"
+	XXBT = "XXBT"
+	XXDG = "XXDG"
+	XXLM = "XXLM"
+	XXMR = "XXMR"
+	XXRP = "XXRP"
+	XXTZ = "XXTZ"
+	XXVN = "XXVN"
+	XZEC = "XZEC"
+	ZCAD = "ZCAD"
+	ZEUR = "ZEUR"
+	ZGBP = "ZGBP"
+	ZJPY = "ZJPY"
+	ZKRW = "ZKRW"
+	ZUSD = "ZUSD"
+)
+
+// Trade types
+const (
+	TradeTypeAll             = "all"
+	TradeTypeAnyPosition     = "any position"
+	TradeTypeClosedPosition  = "closed position"
+	TradeTypeClosingPosition = "closing position"
+	TradeTypeNoPosition      = "no position"
+)
+
+// Ledger types
+const (
+	LedgerTypeAll        = "all"
+	LedgerTypeDeposit    = "deposit"
+	LedgerTypeWithdrawal = "withdrawal"
+	LedgerTypeTrade      = "trade"
+	LedgerTypeMargin     = "margin"
+	LedgerTypeRollover   = "rollover"
+)
+
+// OrderTypes for AddOrder
+const (
+	OTMarket              = "market"
+	OTLimit               = "limit"                  // (price = limit price)
+	OTStopLoss            = "stop-loss"              // (price = stop loss price)
+	OTTakeProfi           = "take-profit"            // (price = take profit price)
+	OTStopLossProfit      = "stop-loss-profit"       // (price = stop loss price, price2 = take profit price)
+	OTStopLossProfitLimit = "stop-loss-profit-limit" // (price = stop loss price, price2 = take profit price)
+	OTStopLossLimit       = "stop-loss-limit"        // (price = stop loss trigger price, price2 = triggered limit price)
+	OTTakeProfitLimit     = "take-profit-limit"      // (price = take profit trigger price, price2 = triggered limit price)
+	OTTrailingStop        = "trailing-stop"          // (price = trailing stop offset)
+	OTTrailingStopLimit   = "trailing-stop-limit"    // (price = trailing stop offset, price2 = triggered limit offset)
+	OTStopLossAndLimit    = "stop-loss-and-limit"    // (price = stop loss price, price2 = limit price)
+	OTSettlePosition      = "settle-position"
 )
